a963980079b1fa482b9fd7be96c62b09b237def7
Silas04420 <silaskolly@gmail.com>
2024-11-13 11:53:11 +0100

    html und css in public

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 app.use(express.json()); // Middleware für JSON-Parsing
 
 const { register, login } = require('./userController');
