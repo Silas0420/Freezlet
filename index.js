76b952648061f0d7adcfdfe5598248ddc9860f64
Jeremias Silas Kolly <J.Kolly.inf24@stud.bbbaden.ch>
2024-11-20 13:15:27 +0100

    registrierung/lernkartenerstellen +

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const session = require('express-session');
 const { register, login, verifyEmail } = require('./userController');
+const { createSet } = require('./setController'); 
 
-
-
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'Freezlettelzeerf01928373645', // Korrekte Schreibweise: 'secret' statt 'ssecret'
+  resave: false,                // Setze auf false, um die Session nicht bei jeder Anfrage zu speichern
+  saveUninitialized: true,      // Setze auf true, um uninitialisierte Sessions zu speichern
+  cookie: { secure: false }     // Setze auf true, wenn du HTTPS verwendest
+}));
 
 app.use(express.json()); // Middleware für JSON-Parsing
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,15 +18,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.get('/emailbestätigung.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'emailbestätigung.html'));
-});
+
 
 // Benutzer-Registrierung und Anmeldung
 app.post('/register', register);
 app.post('/login', login);
-app.get('/verify-email', verifyEmail);
+app.get('/verifizierung', verifyEmail);
 
+app.post('/lernseterstellung', createSet);
 
 const PORT = 3000;
 app.listen(PORT, () => {
