5a63fc42c74be59be8d370424b93e8aa18aac011
Rémy Urech <r.urech.inf24@stud.bbbaden.ch>
2024-12-11 15:08:10 +0100

    Fetch

--- public/home.js
+++ public/home.js
@@ -1,10 +1,8 @@
-    <script>
-        // Lade die Navigation aus einer separaten Datei
-        document.addEventListener("DOMContentLoaded", () => {
             fetch("navigation.html") // Datei mit der Navigation
                 .then((response) => {
                     if (!response.ok) {
-                        throw new Error("Navigation konnte nicht geladen werden");
+                        console.log("fehler1")
+                     throw new Error("Navigation konnte nicht geladen werden");
                     }
                     return response.text();
                 })
@@ -12,7 +10,3 @@
                     document.getElementById("navigation-placeholder").innerHTML = data;
                 })
                 .catch((error) => console.error(error));
-        });
-    </script>
-</body>
-</html>
