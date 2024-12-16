20c08e418bb864fd2368fd842e56b74a2aa3148b
Silas04420 <silaskolly@gmail.com>
2024-12-16 02:26:43 +0100

    +lokale db

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 
 // Erstelle eine Verbindung zur MariaDB-Datenbank
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '94.231.94.146',  // Externe IP-Adresse
-  user: process.env.DB_USER || 'user',  // Dein MariaDB-Benutzername
-  password: process.env.DB_PASSWORD || 'userpassword123*',  // Dein MariaDB-Passwort
+  host: process.env.DB_HOST || 'localhost',  // Externe IP-Adresse
+  user: process.env.DB_USER || 'webuser',  // Dein MariaDB-Benutzername
+  password: process.env.DB_PASSWORD || 'webuserfreezlet',  // Dein MariaDB-Passwort
   database: process.env.DB_NAME || 'freezlet',  // Der Name der Datenbank
   port: process.env.DB_PORT || 3306, 
 });
