c7ede2bb761388a115060dd36138010a9d19b2ce
Silas.schoool <j.kolly.inf24@stud.bbbaden.ch>
2024-12-18 10:36:29 +0100

    fixx2

--- public/lernsets.js
+++ public/lernsets.js
@@ -5,20 +5,19 @@ function loadSets() {
             const setList = document.getElementById('setList');
             if (sets.length == 0) {
                 const h3 = document.createElement('h3');
-                h3.textContent = 'Du hast noch kein Lernset erstellt.'<br>'Klicke auf den Button um ein Lernset zu erstellen.'
-                                const button = document.createElement('button');
-                                button.textContent = 'Erstellen';
-                
-                                // Füge einen EventListener hinzu, der den Benutzer bei Klick weiterleitet
-                                button.addEventListener('click', () => {
-                                    // Hier kannst du den Link oder die Funktion definieren, die nach dem Klick ausgeführt wird
-                                    // Beispiel: Weiterleiten zu einer Seite mit Details zum Ordner
-                                    window.location.href = `/lernseterstellung.html`;
-                                });
-                
-                                // Füge den Button der Liste hinzu
-                                setList.appendChild(h3, button);
-            }
+                h3.textContent = 'Du hast noch kein Lernset erstellt. Klicke auf den Button, um ein Lernset zu erstellen.';
+            
+                const button = document.createElement('button');
+                button.textContent = 'Erstellen';
+            
+                // Füge einen EventListener hinzu, der den Benutzer bei Klick weiterleitet
+                button.addEventListener('click', () => {
+                    window.location.href = `/lernseterstellung.html`;
+                });
+            
+                // Füge die Elemente der Liste hinzu
+                setList.append(h3, button);
+            }            
             sets.forEach(set => {
                 // Erstelle ein Button-Element
                 const button = document.createElement('button');
